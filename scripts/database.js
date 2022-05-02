8be0c55a791d8cce46d3e59eb6bb49d2e45970aa
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-05-02 15:18:04 -0500

    finishing database

--- scripts/database.js
+++ scripts/database.js
@@ -12,26 +12,11 @@ const database = {
         { id: 10, name: "Jody Floyd", status: "active", colonyId: 5 }
     ],
     colonies: [
-        {
-            id: 1,
-            name: "Earth"
-        },
-        {
-            id: 2,
-            name: "Mars"
-        },
-        {
-            id: 3,
-            name: "Venus"
-        },
-        {
-            id: 4,
-            name: "Jupiter"
-        },
-        {
-            id: 5,
-            name: "Saturn"
-        }
+        { id: 1, name: "Earth" },
+        { id: 2, name: "Mars" },
+        { id: 3, name: "Venus" },
+        { id: 4, name: "Jupiter" },
+        { id: 5, name: "Saturn" }
     ],
     miningFacilities: [
         { id: 1, name: "Lucent", status: "inactive" },
@@ -40,6 +25,58 @@ const database = {
         { id: 4, name: "Lumina", status: "active" },
         { id: 5, name: "Ark", status: "active" },
     ],
+    minerals: [
+        { id: 1, name: "Diamond" },
+        { id: 2, name: "Emerald" },
+        { id: 3, name: "Sapphire" },
+        { id: 4, name: "Quartz" },
+    ],
+    //shows inventory quanitity of minerals per colony
+    colonyMinerals: [
+        { id: 1, colonyId: 1, mineralId: 1, quantity: 0 },
+        { id: 2, colonyId: 1, mineralId: 2, quantity: 0 },
+        { id: 3, colonyId: 1, mineralId: 3, quantity: 0 },
+        { id: 4, colonyId: 1, mineralId: 4, quantity: 0 },
+        { id: 5, colonyId: 2, mineralId: 1, quantity: 0 },
+        { id: 6, colonyId: 2, mineralId: 2, quantity: 0 },
+        { id: 7, colonyId: 2, mineralId: 3, quantity: 0 },
+        { id: 8, colonyId: 2, mineralId: 4, quantity: 0 },
+        { id: 9, colonyId: 3, mineralId: 1, quantity: 0 },
+        { id: 10, colonyId: 3, mineralId: 2, quantity: 0 },
+        { id: 11, colonyId: 3, mineralId: 3, quantity: 0 },
+        { id: 12, colonyId: 3, mineralId: 4, quantity: 0 },
+        { id: 13, colonyId: 4, mineralId: 1, quantity: 0 },
+        { id: 14, colonyId: 4, mineralId: 2, quantity: 0 },
+        { id: 15, colonyId: 4, mineralId: 3, quantity: 0 },
+        { id: 16, colonyId: 4, mineralId: 4, quantity: 0 },
+        { id: 17, colonyId: 5, mineralId: 1, quantity: 0 },
+        { id: 18, colonyId: 5, mineralId: 2, quantity: 0 },
+        { id: 19, colonyId: 5, mineralId: 3, quantity: 0 },
+        { id: 20, colonyId: 5, mineralId: 4, quantity: 0 },
+    ],
+    //shows stock quantity of minerals per facility
+    mineralsFacility: [
+        { id: 1, facilityId: 1, mineralId: 1, quantity: 4 },
+        { id: 2, facilityId: 1, mineralId: 2, quantity: 2 },
+        { id: 3, facilityId: 1, mineralId: 3, quantity: 10 },
+        { id: 4, facilityId: 1, mineralId: 4, quantity: 6 },
+        { id: 5, facilityId: 2, mineralId: 1, quantity: 0 },
+        { id: 6, facilityId: 2, mineralId: 2, quantity: 11 },
+        { id: 7, facilityId: 2, mineralId: 3, quantity: 3 },
+        { id: 8, facilityId: 2, mineralId: 4, quantity: 9 },
+        { id: 9, facilityId: 3, mineralId: 1, quantity: 8 },
+        { id: 10, facilityId: 3, mineralId: 2, quantity: 0 },
+        { id: 11, facilityId: 3, mineralId: 3, quantity: 1 },
+        { id: 12, facilityId: 3, mineralId: 4, quantity: 4 },
+        { id: 13, facilityId: 4, mineralId: 1, quantity: 6 },
+        { id: 14, facilityId: 4, mineralId: 2, quantity: 5 },
+        { id: 15, facilityId: 4, mineralId: 3, quantity: 12 },
+        { id: 16, facilityId: 4, mineralId: 4, quantity: 1 },
+        { id: 17, facilityId: 5, mineralId: 1, quantity: 4 },
+        { id: 18, facilityId: 5, mineralId: 2, quantity: 7 },
+        { id: 19, facilityId: 5, mineralId: 3, quantity: 0 },
+        { id: 20, facilityId: 5, mineralId: 4, quantity: 0 },
+    ]
     transientState: {}
 }
 
