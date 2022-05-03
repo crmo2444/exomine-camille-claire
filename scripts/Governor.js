0810d55d701c7966dd95c23749573ffe7d4a835b
Camille Faulkner <camillecfaulkner@gmail.com>
2022-05-03 14:09:19 -0500

    tweaked event listener

--- scripts/Governor.js
+++ scripts/Governor.js
@@ -1,12 +1,14 @@
-import { getGovernors, setGovernor } from "./database.js"
+import { colonyHTML } from "./colony.js"
+import { getGovernors, getTransientState, setGovernor, getColonies, setColony } from "./database.js"
 
 const governors = getGovernors()
+const colonies = getColonies()
 
 
 export const governorHTML = () => {
     let html = `Choose a governor: `
     html += '<select name="governor">'
-
+    
     const listGovernors = governors.map((governor) => {
         return `<option value="${governor.id}">${governor.name}</option>`
     })
@@ -21,6 +23,17 @@ document.addEventListener(
     (event) => {
         if (event.target.name === "governor") {
             setGovernor(parseInt(event.target.value))
+            let transientState = getTransientState()
+            //find governorObject
+            const foundGovernor = governors.find((governor)=> {
+                return governor.id === transientState.governorId
+            })
+            //find the colony whose pk matches governor fk
+            const foundColony = colonies.find((colony)=> {
+                return colony.id === foundGovernor.colonyId
+            })
+            // let newColony = foundColony.name
+            // setColony(newColony)
         }
     }
 )
--- scripts/colony.js
+++ scripts/colony.js
@@ -1,19 +1,20 @@
-import { getColonies, getGovernors, getColonyMinerals, getMinerals } from "./database.js"
+import { getColonies, getGovernors, getColonyMinerals, getMinerals, getTransientState } from "./database.js"
 
 let colonies = getColonies()
+let transientState = getTransientState()
 let governors = getGovernors()
-let colonyMinerals = getColonyMinerals()
-let minerals = getMinerals()
 
 
-export const colonyHTML = () => {
-    let html = ''
-    for (const governor of governors) {
-        for (const colony of colonies) {
-            if (governor.colonyId === colony.id) {
-                html += `<h2>Available Resources in ${colony.name}</h2>`
-            }
-        }
-        return html
-    }
+export const colonyHTML = () => { 
+    let html = ``
+    //console.log(transientState)
+    return html
 }
+
+
+
+
+
+//if governors.id === transientstate.governorId (populated by choosing governor)
+//display the colony name (governor.colonyid ==== colony.id, display colony.name)
+
